adac0ad10dcf7162124fc724df03bd38231e029e
chanin59160553 <59160553@go.buu.ac.th>
2018-09-12 23:28:09 +0700

    ProjectOX_Register_02

--- src/Register.java
+++ src/Register.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Register {
     private JTextField UsernameTxt;
@@ -28,45 +29,55 @@ public class Register {
             public void actionPerformed(ActionEvent e) {
 
                 PreparedStatement pst;
-                if( new String(PasswordField.getPassword()).equals( new String(RepassField.getPassword()))){
+
+                if(UsernameTxt.getText().equals("")||EmailTxt.getText().equals("")||
+                        new String(PasswordField.getPassword()).equals("")||
+                        new String(RepassField.getPassword()).equals("")){
+                    JOptionPane.showMessageDialog(null,"กรุณากรอกข้อมูลให้ครบ !");
+                }else{
+                    if( new String(PasswordField.getPassword()).equals( new String(RepassField.getPassword()))){
+
+                        try{
+                            String serverName = "sql12.freemysqlhosting.net";
+                            String mydatabase = "sql12255832";
+                            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+                            String username = "sql12255832";
+                            String password = "VqusRaY3qH";
+                            Connection connection = DriverManager.getConnection(url, username, password);
+                            connection.createStatement();
+//เช็คว่ามี user นี้หรือยัง
+                            String CheckUser=UsernameTxt.getText();
+                            String sql="SELECT User_Name FROM sql12255832.User where User_Name=?";
+                            pst=connection.prepareStatement(sql);
+                            pst.setString(1,CheckUser);
+                            ResultSet rs = pst.executeQuery();
+
+///
+
+                            boolean russ=rs.next();
+                            if(russ==true){
+                                JOptionPane.showMessageDialog(null,"มี User ซ้ำ");
+                            }else {
+                                InsertUser();
+                            }
 
 
-                    try{
-                        String serverName = "sql12.freemysqlhosting.net";
-                        String mydatabase = "sql12255832";
-                        String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
-                        String username = "sql12255832";
-                        String password = "VqusRaY3qH";
-                        Connection connection = DriverManager.getConnection(url, username, password);
-                        connection.createStatement();
+                        }
 
-                        String sql ="Insert into User(User_Name, User_Email, User_Password) values (?,?,?)";
+                        catch(Exception String) {
+                            JOptionPane.showMessageDialog(null, e);
 
-                        pst=connection.prepareStatement(sql);
-                        pst.setString(1, UsernameTxt.getText());
-                        pst.setString(2, EmailTxt.getText());
-                        pst.setString(3,  new String(PasswordField.getPassword()));
-                        pst.execute();
-
-
-                        JOptionPane.showMessageDialog(null, "ลงทะเบียนเสร็จสิ้น");
-
-
+                        }
 
 
                     }
-
-                    catch(Exception String) {
-                        JOptionPane.showMessageDialog(null, e);
+                    else{
+                        JOptionPane.showMessageDialog(null, "รหัสผ่านไม่ตรงกัน");
 
                     }
-
-
                 }
-                else{
-                    JOptionPane.showMessageDialog(null, "รหัสผ่านไม่ตรงกัน");
 
-                }
+
             }
         });
         clearButton.addActionListener(new ActionListener() {
@@ -79,6 +90,33 @@ public class Register {
             }
         });
     }
+    public void InsertUser(){
+        PreparedStatement pst;
+        try {
+            String serverName = "sql12.freemysqlhosting.net";
+            String mydatabase = "sql12255832";
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+            String username = "sql12255832";
+            String password = "VqusRaY3qH";
+            Connection connection = DriverManager.getConnection(url, username, password);
+            connection.createStatement();
+
+            String sql ="Insert into User(User_Name, User_Email, User_Password) values (?,?,?)";
+
+            pst=connection.prepareStatement(sql);
+            pst.setString(1, UsernameTxt.getText());
+            pst.setString(2, EmailTxt.getText());
+            pst.setString(3,  new String(PasswordField.getPassword()));
+            pst.execute();
+            JOptionPane.showMessageDialog(null,"finish");
+            UsernameTxt.setText("");
+            PasswordField.setText("");
+            RepassField.setText("");
+            EmailTxt.setText("");
+        }catch (Exception String){
+
+        }
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
@@ -90,3 +128,4 @@ public class Register {
         frame.setVisible(true);
     }
 }
+
